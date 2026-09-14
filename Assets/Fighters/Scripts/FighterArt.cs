6153abb9df27e7b434c5836830439e4da86d370f
roma1414 <vinnyroma14@gmail.com>
2026-09-14 14:47:21 -0500

    Added attacker background offset calculation

--- Assets/Fighters/Scripts/FighterArt.cs
+++ Assets/Fighters/Scripts/FighterArt.cs
@@ -6,7 +6,45 @@ using UnityEngine;
 public class FighterArt : ScriptableObject
 {
     [SerializeField]
-    protected Sprite Body;
+    protected Sprite        Body;
+    [SerializeField]
+    protected Sprite        Back;
+    [SerializeField]
+    protected List<Sprite>  TalkingSprites;
+    [SerializeField]
+    protected Sprite        TalkingAngry;
+    protected int           PreviousTalkingSprite = -1;
 
+    public Sprite GetBack() { return Back; }
     public Sprite GetBody() { return Body; }
+    public Sprite GetTalkingAngry() { return TalkingAngry; }
+
+    public Sprite GetTalkingSprite()
+    {
+        if (TalkingSprites.Count == 0)
+        {
+            return null;
+        }
+
+        List<Sprite> possibleSprites = new List<Sprite>();
+        for (int i = 0; i < TalkingSprites.Count; i++)
+        {
+            if (i != PreviousTalkingSprite)
+            {
+                possibleSprites.Add(TalkingSprites[i]);
+            }
+        }
+        
+        if (possibleSprites.Count > 0)
+        {
+            int randomIndex = Random.Range(0, possibleSprites.Count);
+            Sprite selectedSprite = possibleSprites[randomIndex];
+            PreviousTalkingSprite = TalkingSprites.IndexOf(selectedSprite);
+            return selectedSprite;
+        }
+        
+        return TalkingSprites[0];
+    }
+
+    public List<Sprite> GetTalkingSprites() { return TalkingSprites; }
 }
--- Assets/UI/Fight/Scripts/DynamicWindow.cs
+++ Assets/UI/Fight/Scripts/DynamicWindow.cs
@@ -1,34 +1,50 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using System.Linq;
 
 public class DynamicWindow : MonoBehaviour
 {
     [SerializeField] private Fight          Fight;
-    [SerializeField] private Image          FightBackground;
+    [SerializeField] private GameObject     AttackerPanel;
+    [SerializeField] private Image          AttackerBackground;
     [SerializeField] private Image          Attacker;
+    [SerializeField] private GameObject     AttackersPanel;
+    [SerializeField] private Image          AttackersBackground;
+    [SerializeField] private Image          Attackers;
+    public const float ATTACKER_BACKGROUND_MAX_OFFSET = 1330f;
 
-    public void ConfigureFightWindowForAttackers(MoveEvent moveEvent)
+    public void ConfigureFightWindowForAttacker(MoveEvent moveEvent)
     {
         int team = moveEvent.GetFighters()[0].GetTeam();
         if (team == 1)
         {
-            FightBackground.sprite = Fight.GetMapArt().GetBackground_1();
+            AttackerBackground.sprite = Fight.GetMapArt().GetBackground_1();
         }
         else if (team == 2)
         {
-            FightBackground.sprite = Fight.GetMapArt().GetBackground_2();
+            AttackerBackground.sprite = Fight.GetMapArt().GetBackground_2();
         }
         else
         {
-            FightBackground.sprite = Fight.GetMapArt().GetBackground_3();
+            AttackerBackground.sprite = Fight.GetMapArt().GetBackground_3();
         }
 
-        Vector2 position = FightBackground.rectTransform.anchoredPosition;
-        position.x = 0f;
-        FightBackground.rectTransform.anchoredPosition = position;
+        List<Fighter> teamList = Fight.GetTeamList(team);
+        int teamSize = teamList.Count;
+        float offsetPerFighter = 0f;
+        if (teamSize > 1)
+        {
+            offsetPerFighter = ATTACKER_BACKGROUND_MAX_OFFSET / (teamSize - 1);
+        }
+        int fighterIndex = teamList.IndexOf(moveEvent.GetFighters()[0]);
+        float offset = offsetPerFighter * fighterIndex * -1f; // Multiply by -1 to move left for higher index fighters
+
+        Vector2 position = AttackerBackground.rectTransform.anchoredPosition;
+        position.x = offset;
+        AttackerBackground.rectTransform.anchoredPosition = position;
 
         Attacker.sprite = null;
         Sprite attackerSprite = moveEvent.GetFighters()[0].GetArt().GetBody();
@@ -38,13 +54,63 @@ public class DynamicWindow : MonoBehaviour
         }
         else
         {
+            Debug.LogError($"Error! Fighter {moveEvent.GetFighters()[0].GetName()} has no body sprite in ConfigureFightWindowForAttacker!");
+        }
+    }
+
+    public void ConfigureFightWindowForAttackers(MoveEvent moveEvent)
+    {
+        int team = moveEvent.GetFighters()[0].GetTeam();
+        if (team == 1)
+        {
+            AttackersBackground.sprite = Fight.GetMapArt().GetBackground_1();
+        }
+        else if (team == 2)
+        {
+            AttackersBackground.sprite = Fight.GetMapArt().GetBackground_2();
+        }
+        else
+        {
+            AttackersBackground.sprite = Fight.GetMapArt().GetBackground_3();
+        }
+
+        Vector2 position = AttackersBackground.rectTransform.anchoredPosition;
+        position.x = 0f;
+        AttackersBackground.rectTransform.anchoredPosition = position;
+
+        Attackers.sprite = null;
+        Sprite attackerSprite = moveEvent.GetFighters()[0].GetArt().GetBody();
+        if (attackerSprite != null)
+        {
+            Attackers.sprite = attackerSprite;
+        }
+        else
+        {
             Debug.LogError($"Error! Fighter {moveEvent.GetFighters()[0].GetName()} has no body sprite!");
         }
     }
 
     public IEnumerator DisplayMovePreview(MoveEvent moveEvent)
     {
-        ConfigureFightWindowForAttackers(moveEvent);
+        HidePanels();
+        int numberOfFighters = moveEvent.GetFighters().Count;
+        if (numberOfFighters > 1)
+        {
+            ConfigureFightWindowForAttackers(moveEvent);
+            AttackersPanel.SetActive(true);
+        }
+        else
+        {
+            ConfigureFightWindowForAttacker(moveEvent);
+            AttackerPanel.SetActive(true);
+        }
+
         yield return new WaitForSeconds(1f);
+    }
+
+    public void HidePanels()
+    {
+        AttackerPanel.SetActive(false);
+        AttackersPanel.SetActive(false);
     }
 }
